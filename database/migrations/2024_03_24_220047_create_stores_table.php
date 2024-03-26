decd93569a23e45fb33ccdf6e68c4fc436c01a54
fatmatawfeek <fatmatawfeek97@gmail.com>
2024-03-26 20:14:59 +0200

    setup some migrations & models

--- database/migrations/2024_03_24_220047_create_stores_table.php
+++ database/migrations/2024_03_24_220047_create_stores_table.php
@@ -11,14 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('stores', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('phone_number')->nullable()->unique();
-            $table->rememberToken();
+            $table->string('slug')->unique();
+            $table->text('description')->nullable();
+            $table->string('logo_image')->nullable();
+            $table->string('cover_image')->nullable();
+            $table->enum('status', ['active', 'inactive'])->default('active');
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('stores');
     }
 };
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+            'phone_number' => '1234567890',
+        ]);
+    }
+}
