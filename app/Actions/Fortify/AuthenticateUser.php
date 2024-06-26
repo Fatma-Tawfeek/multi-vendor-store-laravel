72b38dd606162239ff85c01a355a08bf785f5766
fatmatawfeek <fatmatawfeek97@gmail.com>
2024-06-26 16:15:19 +0300

    Laravel Fortify: Custom Authentication (Fortify::authenticateUsing)

---
+++ app/Actions/Fortify/AuthenticateUser.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Actions\Fortify;
+
+use App\Models\Admin;
+use Illuminate\Support\Facades\Hash;
+
+class AuthenticateUser
+{
+    public function authenticate($request)
+    {
+        $username = $request->post(config('fortify.username'));
+
+        $user = Admin::where('username', $username)
+            ->orWhere('email', $username)
+            ->orWhere('phone_number', $username)
+            ->where('password', $request->post('password'))
+            ->first();
+
+        if ($user && Hash::check($request->post('password'), $user->password)) {
+            return $user;
+        }
+        return false;
+    }
+}
--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Actions\Fortify\AuthenticateUser;
 use App\Actions\Fortify\CreateNewUser;
 use App\Actions\Fortify\ResetUserPassword;
 use App\Actions\Fortify\UpdateUserPassword;
@@ -52,6 +53,7 @@ class FortifyServiceProvider extends ServiceProvider
 
 
         if (Config::get('fortify.guard') == 'admin') {
+            Fortify::authenticateUsing([new AuthenticateUser, 'authenticate']);
             Fortify::viewPrefix('auth.');
         } else {
             Fortify::viewPrefix('front.auth.');
